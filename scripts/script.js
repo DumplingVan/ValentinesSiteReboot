540a2285d932044ab6cafe4817ac4837af319768
DumplingVan <suvan691@gmail.com>
2026-05-08 22:14:58 -0400

    First Commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const yesButton = document.querySelector(".yes-button");
+const noButton = document.querySelector(".no-button");
+
+function yesfunction(){
+    console.log("Yes button was clicked.")
+
+    yesButton.style.backgroundColor = "red";
+}
+
+function nofunction(){
+    console.log("No button was clicked.")
+}
+
+console.log("Script succesfully loaded!")
+
