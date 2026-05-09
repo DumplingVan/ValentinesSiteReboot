7971eb349d4ef4206cedaaa1fdbebe50f5f06814
DumplingVan <suvan691@gmail.com>
2026-05-08 22:54:56 -0400

    Added popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
-const yesButton = document.querySelector(".yes-button");
-const noButton = document.querySelector(".no-button");
+const yesButton = document.getElementById("yesButton");
+const noButton = document.getElementById("noButton");
+const popup = document.querySelector(".popup")
+const popYes = document.getElementById("popYes")
+const popNo = document.getElementById("popNo")
+
+var noclicks = 0;
 
 function yesfunction(){
     console.log("Yes button was clicked.")
@@ -9,6 +14,22 @@ function yesfunction(){
 
 function nofunction(){
     console.log("No button was clicked.")
+
+    noclicks += 1;
+
+    if (noclicks == 1) {
+        noButton.style.width = "8vh";
+        noButton.style.height = "4vh";
+    }
+    else if (noclicks == 2) {
+        noButton.style.width = "4vh";
+        noButton.style.height = "2vh";
+    }
+    else if (noclicks == 3){
+        noButton.blur()
+        
+        popup.classList.add("active")
+    }
 }
 
 console.log("Script succesfully loaded!")
